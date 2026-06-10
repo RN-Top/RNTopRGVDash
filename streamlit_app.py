184fa7416567cca9cf8cbd67f509fd1dba5ce24f
RN-Top <erintoppe@gmail.com>
2026-06-10 19:11:24 -0400

    Fix input_data DataFrame construction for prediction

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,9 +13,9 @@ rush_hour = st.checkbox("Is it rush hour?")
 
 if st.button("Predict Delivery Time"):
     input_data = pd.DataFrame({
-        'distance_miles': distance,
-        'time_of_day': ,
-        'is_peak': })
+        'distance_miles': [distance],
+        'time_of_day': [time_of_day],
+        'is_peak': [rush_hour] })
     
     prediction = model.predict(input_data)[0]
     st.success(f"🍔 Your food will arrive in about **{int(prediction)} minutes**")
